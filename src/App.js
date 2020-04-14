549ddbd4335c6c3197f6ff7da9231df4b3341561
Michael Verkruyse <mverkruyse@mverkruyse-macbookpro.roam.corp.google.com>
2020-04-14 09:45:37 -0700

    React App Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tmo-logo-v3.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+  <div className="AppPage">
+
+    <div className="AppTopPage">
+      <div>
+        <a class="logoAnchor" href="https://www.t-mobile.com/">
+          <img className="headerLogo" src={logo} alt="T-Mobile"/>
         </a>
-      </header>
+      </div>
+      <div className="headerLinks">
+        <div>
+          <a href="https://dialogflow.com/">Dialogflow</a>
+      </div>
+      <div>
+        <a className="endLink" href="https://www.t-mobile.com/support/account">My Account</a>
+      </div>
+      </div>
     </div>
+
+    <div className="AppMiddlePage">
+      <div>
+        <h1>
+          We’re committed to supporting our customers. Learn how we're responding to COVID-19.
+        </h1>
+      </div>
+    </div>
+
+    <div className="AppBottomPage">
+      <div>
+        <iframe
+          className="iframe"
+          allow="microphone;"
+          width="100%"
+          height="500"
+          src="https://console.dialogflow.com/api-client/demo/embedded/t-mobile"
+          title="My COVID Demo"
+        />
+      </div>
+    </div>
+    
+  </div>
   );
 }
 
